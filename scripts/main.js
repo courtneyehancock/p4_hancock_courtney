7eb013c58a75c3d69ae9d169e0ba98fcbdafa1f2
Courtney Hancock <ceh.courtney@gmail.com>
2019-04-20 14:53:59 -0700

    added javascript typewriter, edited CSS on all pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,12 @@
-var userFeed = new Instafeed({
-    get: 'user',
-    userId: '5762033151',
-    accessToken: '5762033151.1677ed0.be1ef754b4f346499eb8cdffda6a1988',
-    limit: 12,
-});
+//Button External Link
+var ext_link = document.getElementById("external_link");
 
-userFeed.run();var loadButton = document.getElementById('load-more');
-   var feed = new Instafeed({
-       // every time we load more, run this function
-       after: function() {
-           // disable button if no more results to load
-           if (!this.hasNext()) {
-               loadButton.setAttribute('disabled', 'disabled');
-           }
-       },
-   });
-
-   // bind the load more button
-   loadButton.addEventListener('click', function() {
-       feed.next();
-   });
-
-   // run our feed!
-   feed.run();
-
-   var feed = new Instafeed({
-        get: 'charlotte.glancy',
-        userId: '5762033151',
-        filter: function(image) {
-            return image.tags.indexOf('cinnamonrolls') >= 0;
-        }
-    });
-    feed.run();
-
-/*$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
-*/
+ext_link.addEventListener("click",
+	function(event) {
+		var is_sure = confirm("This link will take you to an external page. Are you sure you want to leave?");
+		if (is_sure === false) {
+			event.preventDefault(); //this prevents the default click behavior of sending someone away from your site
+			alert("Okay! You can stay.");
+		}
+	}, false
+);
